4f2bff57422ed1b9d2d0c914df5b3226c31632fe
David Tonoian <tonoian.ds17@gmail.com>
2025-02-12 16:39:21 -0600

    added description to notebook and load routine

--- cirbe/load.py
+++ cirbe/load.py
@@ -75,16 +75,18 @@ def load(trange=['2024-09-21', '2024-09-22'],
     for name in files:
         filename = os.path.join(CONFIG['local_data_dir'], name)
         #fixing Epoch variable attribute including "UTC" where it should not
-        data = xr.load_dataset(filename)
-        if data.Epoch.attrs['UNITS'][:3] == 'UTC':
-            logging.info("Fixing 'Epoch' variable for "+filename)
-            data.Epoch.attrs['UNITS'] = data.Epoch.attrs['UNITS'][4:]
-            out_files.append(f"{filename[:-3]}_fixepoch.nc")
-            data.to_netcdf(out_files[-1])
-            logging.info(f"Saved in {out_files[-1]}")
-        else:
-            logging.info("No 'UTC' in the 'Epoch' attribute or it was already removed.")
-
+        try:
+            data = xr.load_dataset(filename)
+            if data.Epoch.attrs['UNITS'][:3] == 'UTC':
+                logging.info("Fixing 'Epoch' variable for "+filename)
+                data.Epoch.attrs['UNITS'] = data.Epoch.attrs['UNITS'][4:]
+                out_files.append(f"{filename[:-3]}_fixepoch.nc")
+                data.to_netcdf(out_files[-1])
+                logging.info(f"Saved in {out_files[-1]}")
+            else:
+                logging.info("No 'UTC' in the 'Epoch' attribute or it was already removed.")
+        except:
+            logging.warning(f"Cannot open file {name} to fix epoch. Either it is already fixed or there is some other problem")
 
     if not files:
         logging.error(f"CIRBE LOAD: NO netCDF FILE FOUND! check file {remote_names}")
@@ -110,15 +112,58 @@ def load(trange=['2024-09-21', '2024-09-22'],
 
 
 def reptile2(trange=['2024-09-21', '2024-09-22'],
-             level='l1', 
+             type = 'flux', 
              suffix='', 
              version = 'v2',
-             downloadonly=False, #not yet realised
-             notplot=False, #not yet realised
-             no_update=False, #not yet realised
+             downloadonly=False, #not yet implemented
+             notplot=False, #not yet implemented
+             no_update=False, #not yet implemented
              time_clip=False,
-             force_download=False #not yet realised
+             force_download=False #not yet implemented
             ):
+    """
+    This function loads data from the CIRBE\REPTile-2 instrument and process it into electron spectra.
+
+    Parameters for Load Routine
+    ---------------------------
+        trange : list of str
+            Time range of interest [starttime, endtime]. Format can be
+            ['YYYY-MM-DD','YYYY-MM-DD'] or ['YYYY-MM-DD/hh:mm:ss','YYYY-MM-DD/hh:mm:ss']
+            Default: ['2022-08-19', '2022-08-19']
+
+        type: str, optional
+            Calibrated data type of L1 data. Only option 'flux' for now.
+
+        version: str, optional.
+            Version of L1 data. Options are 'v1' and 'v2'.
+            Default: 'v2'.
+
+        downloadonly: bool, optional
+            Not implemented yet. If True, only downloads the CDF files without loading them into tplot variables. 
+            Default: False.
+
+        notplot: bool, optional
+            Not implemented yet. If True, returns data in hash tables instead of creating tplot variables. 
+            Default: False.
+
+        no_update: bool
+            Not implemented yet. If True, loads data only from the local cache.
+            Default: False.
+
+        time_clip: bool
+            If True, clips the variables to the exact range specified in the trange. 
+            Default: True.
+
+        force_download: bool
+            Not implemented yet. Download file even if local version is more recent than server version
+            Default: False
+    
+    
+    Returns
+    -------
+        list of str
+            List of tplot variables created.
+    """
 
     tvars = load(trange=trange,
              level='l1', #correct since we are creating l1 data in this function
@@ -131,7 +176,7 @@ def reptile2(trange=['2024-09-21', '2024-09-22'],
              force_download=False
             )
 
-    if level.lower() == 'l2':
+    if type == 'flux':
         logging.info("CIRBE REPTile-2 L2: START PROCESSING.")
         
         t, ecounts = pyspedas.get_data('Ebins_RNG')
@@ -146,12 +191,12 @@ def reptile2(trange=['2024-09-21', '2024-09-22'],
         #Ebins_RNG: Counts for range electron channels 1:50 (or channels 61:110 for the combined channels)
         eflux = etmp/gde[None, (channel>60)&(channel<111)] #Ebins_RNG: Counts for range electron channels 1:50 (or channels 61:110 for the combined channels)
 
-        pyspedas.store_data('cirbe_eflux_adapted',data={'x':t, 'y':(eflux),'v':(energy[60:110])})
-        pyspedas.options('cirbe_eflux_adapted',opt_dict ={'spec': True,'yrange':[0.3,5.],'ystyle':1,'ylog':True,
+        pyspedas.store_data('cirbe_efluxe_adapted',data={'x':t, 'y':(eflux),'v':(energy[60:110])})
+        pyspedas.options('cirbe_efluxe_adapted',opt_dict ={'spec': True,'yrange':[0.3,5.],'ystyle':1,'ylog':True,
                         'ytitle':'Energy [MeV]','zlog':True,
                         'zticklen':-0.5,
                         'zrange':[10,1e6],
                         'ztitle':'Flux\n[/cm^2/s/str/MeV]'})
-        tvars += ['cirbe_eflux_adapted']
+        tvars += ['cirbe_efluxe_adapted']
 
     return tvars
